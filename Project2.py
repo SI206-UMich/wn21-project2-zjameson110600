4588404a1ce4ba5b9b6e1039a51206d1f513ed0f
Zita Jameson <zjameson@umich.edu>
2021-03-21 15:29:42 -0400

    first method, some tests

--- Project2.py
+++ Project2.py
@@ -4,7 +4,7 @@ import re
 import os
 import csv
 import unittest
-
+#worked with: grace coleman
 
 def get_titles_from_search_results(filename):
     """
@@ -14,8 +14,13 @@ def get_titles_from_search_results(filename):
 
     [('Book title 1', 'Author 1'), ('Book title 2', 'Author 2')...]
     """
-
-    pass
+    url = "search_results.htm"
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    tags = soup.find_all('a')
+    for tag in tags:
+        print(tag.get('href', None))
+    return True
 
 
 def get_search_links():
@@ -116,50 +121,54 @@ class TestCases(unittest.TestCase):
 
         # check that the last title is correct (open search_results.htm and find it)
 
-    def test_get_search_links(self):
+    def test_get_search_links(self:)
         # check that TestCases.search_urls is a list
-
+        self.assertTrue(type(TestCases.search_urls), <type list>)
         # check that the length of TestCases.search_urls is correct (10 URLs)
-
+        self.assertEqual(len(TestCases.search_urls), 10)
 
         # check that each URL in the TestCases.search_urls is a string
+        for x in TestCases.search_urls:
+            self.assertTrue(type(x) == <type string>)
         # check that each URL contains the correct url for Goodreads.com followed by /book/show/
-
+        for x in TestCases.search_urls:
+            self.assertTrue("/book/show/" in x)
 
     def test_get_book_summary(self):
         # create a local variable – summaries – a list containing the results from get_book_summary()
         # for each URL in TestCases.search_urls (should be a list of tuples)
-
+        summaries = self.get_book_summary()
         # check that the number of book summaries is correct (10)
-
+        self.assertEqual(len(summaries), 10)
             # check that each item in the list is a tuple
-
+        for x in summaries:
+            self.assertTrue(type(x) == <tuple>)
             # check that each tuple has 3 elements
-
+            self.assertEqual(len(x), 3)
             # check that the first two elements in the tuple are string
-
+            self.assertTrue(type(x[0][:1]) == <type str>)
             # check that the third element in the tuple, i.e. pages is an int
-
+            self.assertTrue(type(x[0][2]) == <type int>)
             # check that the first book in the search has 337 pages
-
+            self.assertEqual(x[0][2], 337)
 
     def test_summarize_best_books(self):
         # call summarize_best_books and save it to a variable
-
+        summarize = self.summarize_best_books()
         # check that we have the right number of best books (20)
-
+        self.assertEqual(len(summarize), 20)
             # assert each item in the list of best books is a tuple
-
+        for x in summarize:
             # check that each tuple has a length of 3
-
+            self.assertEqual(len(x), 3)
         # check that the first tuple is made up of the following 3 strings:'Fiction', "The Midnight Library", 'https://www.goodreads.com/choiceawards/best-fiction-books-2020'
-
+        self.assertEqual(x[0], ('Fiction', "The Midnight Library", 'https://www.goodreads.com/choiceawards/best-fiction-books-2020'))
         # check that the last tuple is made up of the following 3 strings: 'Picture Books', 'A Beautiful Day in the Neighborhood: The Poetry of Mister Rogers', 'https://www.goodreads.com/choiceawards/best-picture-books-2020'
-
+        self.assertEqual(x[-1], ('Picture Books', 'A Beautiful Day in the Neighborhood: The Poetry of Mister Rogers', 'https://www.goodreads.com/choiceawards/best-picture-books-2020'))
 
     def test_write_csv(self):
         # call get_titles_from_search_results on search_results.htm and save the result to a variable
-
+        titles = self.get_titles_from_search_results()
         # call write csv on the variable you saved and 'test.csv'
 
         # read in the csv that you wrote (create a variable csv_lines - a list containing all the lines in the csv you just wrote to above)
